b334c59ca3b956bab50bb70029806b08eeeaebb5
Екатерина Яковлева <yallanaba@yandex.ru>
2024-12-23 16:20:13 +0300

    initiate brain-progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,42 @@
+import play from '../index.js';
+import { findGcd, getRandomNumber } from '../utils.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+const numberScope = 100;
+
+const prepareRoundData = (numberScope) => {
+  return {
+    randomNumberOne: getRandomNumber(numberScope),
+    randomNumberTwo: getRandomNumber(numberScope),
+  };
+};
+
+const generateAnswer = (gameData) => {
+  const correctAnswer = findGcd(
+    gameData.randomNumberOne,
+    gameData.randomNumberTwo
+  ).toString();
+  return correctAnswer;
+};
+
+const generateRound = (gameData) => {
+  const question = `Question: ${gameData.randomNumberOne} ${gameData.randomNumberTwo}`;
+  const answer = generateAnswer(gameData);
+  return {
+    question: question,
+    correctAnswer: answer,
+  };
+};
+
+const playBrainProgression = () => {
+  const game = {
+    rules,
+    numberScope,
+    prepareRoundData,
+    generateRound,
+  };
+
+  play(game);
+};
+
+export default playBrainProgression;
